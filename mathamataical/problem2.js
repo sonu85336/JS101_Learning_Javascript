e7ab016839869679747d044b4df398617e0662e5
Your Name <you@example.com>
2022-09-04 11:55:42 -0700

    mathmatical

---
+++ mathamataical/problem2.js
@@ -0,0 +1,10 @@
+//Problem 2: If a person is allowed to drive in India print "Apply for a license" or "NA".
+
+let age = 17;
+let allow_age = 18;
+if (age >= allow_age) {
+  console.log("Apply for a license");
+}
+else {
+  console.log("NA");
+}
---
+++ mathamataical/problem9.js
@@ -0,0 +1,2 @@
+//Problem 4: Given 3 numbers (all different values), print which is greatest
+
