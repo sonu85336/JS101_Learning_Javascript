364d85c798ef634c90e1ec08669f79bbfe99f718
Your Name <you@example.com>
2022-09-05 13:59:32 -0700

    screenshot

--- prob3.js
+++
@@ -1,9 +0,0 @@
-//You are given two numbers, stored in the variable with the following names
-//one, two  /If both the values are even, printEven/Else if both the values are odd, printOdd/else printEven-Odd
-
-let one = 3;
-let two = 6;
-
-
-
-
